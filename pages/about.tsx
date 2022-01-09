91ec5e93c55066b6ed9dd219e3c73a0a3497224a
Connor Barrett <connor.steven.barrett@gmail.com>
2022-01-09 17:44:26 -0600

    10 album images falling randomly

---
+++ pages/about.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const About: NextPage = () => {
+   return (
+      <div>
+         about page
+         <Avatar/>
+      </div>
+   )
+}
+
+function Avatar() {
+   return <Image src="/vercel.svg" alt="me" width="64" height="64" />
+}
